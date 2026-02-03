042e02c804ee378b91b2a7ba0554e683c2e5b1db
FRANCIELE SANTOS DE JESUS <franciele.sjesus2@senacsp.edu.br>
2026-02-03 19:11:51 -0300

    Primeira Aula Programador de Sistemas -Revisão

---
+++ Aula1-Revisão/Aula1-Revisão/Program.cs
@@ -0,0 +1,46 @@
+﻿//SOMAR DOIS NÚMEROS
+
+//DECLARAÇÃO DE VARIÁVEIS | ENTRADA
+
+//Instanciamento (CHAMAR AS VARIÁVEIS)
+
+//using Aula1_Revisão;
+
+//Soma x = new Soma();
+
+//Console.WriteLine("Digite um número inteiro: ");
+//x.a= int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Digite outro número inteiro: ");
+//x.b = int.Parse(Console.ReadLine());
+
+////PROCESSAMENTO
+//x.resultado = x.a + x.b;
+
+////SAIDA
+//Console.WriteLine("O resultado da soma é: " + x.resultado);
+
+
+////EXERCÍCIO
+//// Escreva um programa que peça uma altura em metros e converta para cm.
+
+//DECLARAÇÃO DE VARIÁVEIS(ATRIBUTOS)
+
+using Aula1_Revisão;
+
+Converter y = new Converter();
+
+//ENTRADA
+Console.WriteLine("Digite sua altura em Metros:");
+y.alturaM = double.Parse(Console.ReadLine());
+
+
+//PROCESSAMENTO
+y.converter = y.alturaM * 100;
+
+
+//SAIDA
+Console.WriteLine("Sua altura em Centimetros: " + y.converter);
+
+//===============================================================
+
---
+++ Aula1-Revisão/Aula1-Revisão/Soma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1_Revisão
+{
+    internal class Soma
+    {
+        public int a, b;
+        public int resultado;
+
+
+    }
+}
