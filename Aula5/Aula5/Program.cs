4a4bfe0715061e35c889634af1d6002201eace85
FRANCIELE SANTOS DE JESUS <franciele.sjesus2@senacsp.edu.br>
2026-02-06 21:53:13 -0300

    Exercícios de While com Classe e Função

---
+++ Aula5/Aula5/Program.cs
@@ -0,0 +1,111 @@
+﻿//AULA V - LAÇOS
+
+
+////VARIÁVEIS
+//int C;
+
+////Processo 
+//C = 0;
+
+//while(C<3)
+//{
+//    Console.WriteLine("SENAC");
+//    C++;
+//}
+
+//exercícios
+//Faremos um programa que escreva os números de 1 à 100
+//Faremos um programa que escreva os números de 50 à 100
+//Faremos um programa que faça a contagem regressiva de 10 à 0
+//e no fim dizer fogo;
+//Faça um programa que peça um valor positivo do usuário e 
+//programa deve contar do 1 até o número colocado pelo usuário;
+
+//EXERCÍCIO I
+
+//int C, contador, num;
+
+////Processo 
+//C = 1;
+
+
+//while (C <= 100)
+//{
+//    Console.WriteLine(C);
+//    C++;
+//}
+
+//EXERCÍCIO II
+
+//contador = 50;
+
+//while (contador <= 100)
+//{
+//    Console.WriteLine(contador);
+//    contador++;
+//}
+
+//EXERCÍCIO III
+
+//contador = 10;
+
+
+//while (contador > 0)
+//{
+//    Console.WriteLine(contador);
+//    contador--;
+//}
+//Console.WriteLine("Fogos");
+
+
+
+//EXERCÍCIO IV
+
+//contador = 1;
+
+//Console.WriteLine("Escreva um número positivo");
+//num = int.Parse(Console.ReadLine());
+
+//while (contador<=num )
+//{
+//    Console.WriteLine(contador);
+//    contador++;
+//}
+
+//EXERCÍCIOS COM FUNÇÃO
+
+using Aula5;
+
+//EXERCÍCIO I
+
+//Contador contador = new Contador();
+//contador.Contagem();
+
+//EXERCÍCIO II
+
+//contador.Contar();
+
+//EXERCÍCIO III
+
+//contador.Regressiva();
+
+//EXERCÍCIO IV
+
+//contador.ChamarContagem();
+
+//EXERCÍCIO
+
+//Elabore um programa que mostre os resultados da
+//Tabuada de um número qualquer, a qual deve ser
+//apresentada de acordo com sua forma tradicional
+
+//Tabuada tabuada=new Tabuada();
+//tabuada.ChamarTabuada();
+
+
+//EXERCÍCIO
+
+//Vamos fazer um sistema
+
+Sistema sistema=new Sistema();
+sistema.Cadastro();
---
+++ Aula5/Aula5/Sistema.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula5
+{
+    internal class Sistema
+    {
+
+        public void Cadastro()
+        {
+            while (true)
+            {
+                Console.WriteLine("== Sistema ==");
+                Console.WriteLine("(1) Cadastro");
+                Console.WriteLine("(2) Login");
+                Console.WriteLine("(0) Sair");
+                Console.WriteLine("Escolha");
+                int op =int.Parse(Console.ReadLine());  
+
+                switch(op)
+                {
+                    case 1:
+                        Console.WriteLine("Função Cadastro");
+                        break;
+                    case 2:
+                        Console.WriteLine("Função Login");
+                        break;
+                    case 0:
+                        Console.WriteLine("Função Sair");
+                        return;
+                    default:
+                        Console.WriteLine("Opção Inválida");
+                        break;
+                }
+                Console.WriteLine("Pressione ENTER para continuar...");
+                Console.ReadLine();
+
+                Console.Clear();
+            }
+        }
+    }
+}
