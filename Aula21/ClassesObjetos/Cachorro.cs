8e964ab12a1a1f770d2609eea8df5329f5ee42e5
FRANCIELE SANTOS DE JESUS <franciele.sjesus2@senacsp.edu.br>
2026-03-10 22:01:18 -0300

    Exercícios com FUNÇÕES E METODOS

---
+++ Aula21/ClassesObjetos/Cachorro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesObjetos
+{
+    internal class Cachorro
+    {
+        public string Nome;
+
+        public void Latir()
+        {
+            Console.WriteLine($"{Nome} diz: auu au au!");
+            Console.WriteLine("       /^ ^\\        ");
+            Console.WriteLine("      / 0 0 \\        ");
+            Console.WriteLine("      V\\ Y /V       ");
+            Console.WriteLine("       / -  \\       ");
+            Console.WriteLine("      /      |      ");
+            
+        }
+    }
+
+}
---
+++ Aula21/ClassesObjetos/Calculos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesObjetos
+{
+    internal class Calculos
+    {
+        public double A;
+        public double B;
+
+        public void SomarSEMretorno()
+        {
+            double resultado = A + B;
+
+        }
+
+        public double SomarCOMretorno()
+        {
+            double resultado = A + B;
+            return resultado;
+        }
+    }
+}
